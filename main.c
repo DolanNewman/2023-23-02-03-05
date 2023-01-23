c215c20947097bfe36819980dc80fc3b696bc0dd
Dolan Newman <>
2023-01-23 15:17:01 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+// TODO: Write a program that reverses a character array
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+// Maybe make a pointer point to the end of an array?
+//no clue how to code in C 
+// Point to the end of an array, and add the characters from the end of the array to another empty array, and print the once was empty array... out into the console.
+// I do not know C syntax. How do I do that
+// Update. I don't know how to code in C still.
+
+  
+  char str1 [] = "word";
+  char word* = str1; // now how do i get it to point to the end of this array?
+  return 0;
+}
